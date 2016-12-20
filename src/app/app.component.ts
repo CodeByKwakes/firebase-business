1c8a447685a0e1f3f5cdc83f8d8506e23cf707bc
KwakesProject <kwakes@me.com>
2016-12-20 15:56:24 +0000

    full crud firebase app created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,18 @@ export class AppComponent implements OnInit {
   appState: string;
   activeKey: string;
 
+  activeCompany: string;
+  activeCategory: string;
+  activeYearsInBusiness: string;
+  activeDescription: string;
+  activePhone: string;
+  activeEmail: string;
+  activeStreetAddress: string;
+  activeCity: string;
+  activeState: string;
+  activeZipcode: string;
+
+
   constructor(private _firebaseService: FirebaseService) { }
 
   ngOnInit() {
@@ -34,7 +46,7 @@ export class AppComponent implements OnInit {
   }
 
   filterCategory(category) {
-    this._firebaseService.getBusinesses(category).subscribe(businesses => this.businesses = businesses)
+    this._firebaseService.getBusinesses(category).subscribe(businesses => this.businesses = businesses);
   }
 
   addBusiness(
@@ -49,7 +61,7 @@ export class AppComponent implements OnInit {
     city: string,
     zipcode: string
   ) {
-    let created_at = new Date().toString()
+    let created_at = new Date().toString();
     let newBusiness = {
       company: company,
       category: category,
@@ -62,9 +74,47 @@ export class AppComponent implements OnInit {
       state: state,
       zipcode: zipcode,
       created_at: created_at
-    }
-    console.log(newBusiness)
-    this._firebaseService.addBusiness(newBusiness)
+    };
+    console.log(newBusiness);
+    this._firebaseService.addBusiness(newBusiness);
+    this.changeState('default');
+  }
+
+  showEdit(business) {
+    this.changeState('edit', business.$key);
+
+    this.activeCompany = business.company;
+    this.activeCategory = business.category;
+    this.activeYearsInBusiness = business.years_in_business;
+    this.activeDescription = business.description;
+    this.activePhone = business.phone;
+    this.activeEmail = business.email;
+    this.activeStreetAddress = business.street_address;
+    this.activeCity = business.city;
+    this.activeState = business.state;
+    this.activeZipcode = business.zipcode;
+  }
+
+  updateBusiness() {
+    let updBusiness = {
+      company: this.activeCompany,
+      category: this.activeCategory,
+      years_in_business: this.activeYearsInBusiness,
+      description: this.activeDescription,
+      phone: this.activePhone,
+      email: this.activeEmail,
+      street_address: this.activeStreetAddress,
+      city: this.activeCity,
+      state: this.activeState,
+      zipcode: this.activeZipcode
+    };
+    this._firebaseService.updateBusiness(this.activeKey, updBusiness);
+    this.changeState('default');
+
+  }
+
+  deleteBusiness(key) {
+    this._firebaseService.deleteBusiness(key);
     this.changeState('default')
   }
 }
