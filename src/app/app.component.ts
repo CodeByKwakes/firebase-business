125f5506e9e6f6848e9047110c2cdbd47eb37d8a
KwakesProject <kwakes@me.com>
2016-12-20 12:09:31 +0000

    firebase service is set up and displaying business company on screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { FirebaseService, Business, Category } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
-  items: FirebaseListObservable<any[]>;
-  constructor(af: AngularFire) {
-    this.items = af.database.list('items');
-    console.log(this.items);
+  businesses: Business[];
+  categories: Category[];
+
+  constructor(private _firebaseService: FirebaseService) {}
+
+  ngOnInit() {
+    this._firebaseService.getBusinesses()
+    .subscribe(businesses => this.businesses = businesses);
+
+     this._firebaseService.getCategories()
+    .subscribe(categories => this.categories = categories);
   }
 }
