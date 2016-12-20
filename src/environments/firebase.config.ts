8a0c6cd86cf0661f975f8f09540a499feede9c7b
KwakesProject <kwakes@me.com>
2016-12-20 10:29:50 +0000

    added more to config settings

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCz_P1S2sFTgDrtAl073BXl7Me2aeSNZcA',
+  authDomain: 'fire-business-contacts.firebaseapp.com',
+  databaseURL: 'https://fire-business-contacts.firebaseio.com',
+  storageBucket: 'fire-business-contacts.appspot.com'
+};
